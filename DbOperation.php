c631879741701397e6b33913c7338b30db0172d3
arorash <56073547+arorash@users.noreply.github.com>
2020-09-06 12:52:57 +0530

    login and user already exist features are added

--- DbOperation.php
+++ DbOperation.php
@@ -8,15 +8,44 @@
             $this->con = $db->connect();
         }
 
-        function create_user($username,$password){
-            $pass = md5($password);
-            $stmt = $this->con->prepare("insert into login(username,password) values(?,?);");
-            $stmt->bind_param("ss",$username,$password);
-            if($stmt->execute()){
-                return true;
+        public function create_user($username,$password){
+            if($this->isUserExist($username)){
+                return 0;
             }
             else{
-                return false;
+
+                $pass = md5($password);
+                $stmt = $this->con->prepare("insert into login(username,password) values(?,?);");
+                $stmt->bind_param("ss",$username,$password);
+                if($stmt->execute()){
+                    return 1;
+                }
+                else{
+                    return 2;
+                }
             }
+        }
+
+        public function userLogin($username,$password){
+            $stmt = $this->con->prepare("select id from login where username=? and password=?;");
+            $stmt->bind_param("ss",$username,$password);
+            $stmt->execute();
+            $stmt->store_result();
+            return $stmt->num_rows > 0;
+        }
+
+        public function getUserDetails($username){
+            $stmt = $this->con->prepare("select * from login where username=?;");
+            $stmt->bind_param("s",$username);
+            $stmt->execute();
+            return $stmt->get_result()->fetch_assoc();
+        }
+
+        private function isUserExist($username){
+            $stmt = $this->con->prepare("select id from login where username = ?;");
+            $stmt->bind_param("s",$username);
+            $stmt->execute();
+            $stmt->store_result();
+            return $stmt->num_rows > 0;
         }
     }
